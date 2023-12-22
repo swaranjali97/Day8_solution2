aa538b7940708de8b575a707b955f24295446b27
swaranjali97 <swaranjalipatilcdac@gmail.com>
2023-12-22 15:20:05 +0530

    uc1

---
+++ EmployeeAttendence.java
@@ -0,0 +1,17 @@
+
+public class EmployeeAttendence {
+
+	
+		public static void main(String[] args) {
+			int FULL_TIME=1;
+			double emp_check=Math.floor(Math.random()*10)%2;
+			if(emp_check==1)
+				System.out.println("employee is present");
+			else
+				System.out.println("employee is absent");
+
+		}
+
+	}
+
+
